e5e4a75df3ae728e4195ad3c1c3c7157970ccca5
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-05 17:39:24 +0300

    Crud operation for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,9 @@ class BookingsController < ApplicationController
         booking=Booking.create(booking_params)
         render json: booking
     end
+    def index
+        render json: Booking.all
+    end
     private
     def booking_params
         params.permit(:name,:house_id,:check_in_date,:check_out_date,:No_of_people,:total)
