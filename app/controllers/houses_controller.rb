c119db4d6cf492c77633c9721cd0277f15d2e241
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-05 17:34:16 +0300

    CRUD operations for houses

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,32 @@
 class HousesController < ApplicationController
+    def index
+        render json: House.all, status: :ok
+    end
+    def show
+        house=House.find(params[:id])
+        render json: house, status: :found
+    end
+    def create
+        house=House.create(house_params)
+        render json: house, status: :created
+    end
+    def destroy
+        house=House.find(params[:id])
+        house.destroy()
+        head :no_content
+    end
+    def update
+        house=House.find(params[:id])
+        if house
+            house.update(house_params)
+            render json: house
+        else
+            render json: {error: "house not found"}
+            
+        end
+    end
+    private
+    def house_params
+        params.permit(:house_type,:image,:beds,:price,:location,:description,:ratings,:capacity,:owner_id)
+    end
 end
