5438805eb474e59d9b75a53575819c6c879c0f61
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-05 17:34:45 +0300

    Crud operations for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+    def create
+        user=User.create(user_params)
+        render json: user
+    end
+    def show
+        user=User.find(params[:id])
+        if user
+            render json: user
+        else
+            render json: {error: "User not found"}
+        end
+    end
+    private
+    def user_params
+        params.permit(:id,:name,:email,:phone_number,:house_id)
+    end
 end
