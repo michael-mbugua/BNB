381a68ad1dd6c39ffc08885b9d877e92ca185008
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-05 21:25:44 +0300

    minor changes

--- db/migrate/20221205113351_create_users.rb
+++ db/migrate/20221205113351_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.text :email
-      t.integer :phone_number
+      t.integer :phoneNumber
       t.integer :house_id
       t.timestamps
     end
--- db/migrate/20221205114512_create_bookings.rb
+++ db/migrate/20221205114512_create_bookings.rb
@@ -2,10 +2,9 @@ class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
       t.string :name
-      t.integer :house_id
-      t.date :check_in_date
-      t.date :check_out_date
-      t.integer :No_of_people
+      t.date :checkIn
+      t.date :checkOut
+      t.integer :persons
       t.integer :total
       t.timestamps
     end
