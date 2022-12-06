6993a0564e3c34e22f75be5833e7cb4ef7ef6577
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-06 11:21:11 +0300

    custom routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   resources :bookings ,only:[:create,:index]
-  resources :owners ,only:[:index]
+  resources :owners ,only:[:index,:show]
   resources :users
   resources :houses
   get '.houses/:house_type' ,to: "house#show"
-  get '/login',to: "sessions#create"
+  # users login in and staying logged in
+  get '/login',to: "sessions#create"  
   get '/me',to: "users#show"
+  # users loging out route
   get '/logout',to: "sessions#destroy"
+
+  # admin login in
+  get '/admin', to: "admins#create"
+  get '/me',to: "owners#show"
+  # admin logging out
+  get '/logout',to: "admins#destroy"
+
 end
