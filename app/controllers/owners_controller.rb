e527dd7ebc0ba0c936dfa3b1be844ec76c4b9f16
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-06 11:19:40 +0300

    log in owners

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,4 +2,12 @@ class OwnersController < ApplicationController
     def index
         render json: Owner.all, status: :ok
     end
+    def show
+        owner=Owner.find_by(id: session[:owner_id])
+        if owner
+            render json: owner
+        else
+            render json: {error: "not authorized"},status: :unauthorized
+        end
+    end
 end
