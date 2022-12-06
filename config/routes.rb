9754e2cd2b11b40cbd6b38eabec3c4cacc55ea92
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-06 09:09:00 +0300

    custom routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :bookings
-  resources :owners
+  resources :bookings ,only:[:create,:index]
+  resources :owners ,only:[:index]
   resources :users
   resources :houses
+  get '.houses/:house_type' ,to: "house#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
