2b8a9e546a61cd62aa587f731db98f6bfaac03f4
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-10 11:14:17 +0300

    submitting right folder

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React,{useEffect,useState} from 'react';
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage'
+import Register from './Register';
+import Login from './Login';
+import Book from './Book';
+import Admin from './Admin';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+
+  
+
+  return (
+
+    // BEM
+    <div className="app">
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/register">
+            <Register/>
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/booking">
+            <Book />
+          </Route>
+          <Route path="/admin">
+            <Admin/>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </ Router>
+    </div>
+  );
+}
+
+export default App;
